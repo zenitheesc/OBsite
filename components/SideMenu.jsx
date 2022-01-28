ba890a8cdb42a6666a3690bdb5592d8119c9cad3
Pedro Torrente <pedrotorrente@usp.br>
2022-01-28 11:25:47 -0300

    side menu and header structure created

---
+++ components/SideMenu.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "../styles/SideMenu.module.css";
+
+export default function SideMenu() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.headerBox}> Cabeçalho </div>
+    </div>
+  );
+}
