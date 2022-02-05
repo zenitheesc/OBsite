03467a1f4a67a6ffbaaa14caf2e67d27807fc8aa
MichelH4cker <michel.hecker@usp.br>
2022-02-05 10:32:33 -0300

    merge all branchs in side-menu - some bugs exists

--- components/SideMenu.jsx
+++ components/SideMenu.jsx
@@ -1,27 +1,20 @@
 import React from "react";
 import styles from "../styles/SideMenu.module.css";
-<<<<<<< HEAD
 import DataTeam from "./DataTeam";
 import HorizontalLine from "./HorizontalLine";
 import Footer from "./Footer";
-=======
 import PackageGrid from "./PackageGrid";
 import PackageButtons from "./PackageButtons";
->>>>>>> origin/package-buttons
 
 export default function SideMenu() {
   return (
     <div className={styles.container}>
-<<<<<<< HEAD
       <div className={styles.headerBox}>
         <DataTeam></DataTeam>
       </div>
 
       <HorizontalLine></HorizontalLine>
 
-      <Footer></Footer>
-=======
-      <div className={styles.headerBox}> Cabeçalho </div>
       <PackageGrid>
         <PackageButtons time="15:32:51" />
         <PackageButtons time="15:32:51" />
@@ -32,8 +25,9 @@ export default function SideMenu() {
         <PackageButtons time="15:32:51" />
         <PackageButtons time="15:32:51" />
       </PackageGrid>
->>>>>>> origin/package-buttons
-    </div>
 
+      <Footer></Footer>
+
+    </div>
   );
 }
