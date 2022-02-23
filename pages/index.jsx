362e225895b853966a5da55b188464a1a20e467c
MichelH4cker <michel.hecker@usp.br>
2022-02-23 19:31:54 -0300

    homepage: create div container

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 import SideMenu from "../components/SideMenu";
+import styles from "../styles/Home.module.css";
 //  import Head from "next/head";
-//  import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  return <SideMenu> </SideMenu>;
+  return(
+    <div className={styles.container}>
+      <SideMenu> </SideMenu>;
+    </div>
+  );
 }
