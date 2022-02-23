f062c8304db153b543a1662cf4aae94e30679c98
MichelH4cker <michel.hecker@usp.br>
2022-02-23 20:16:02 -0300

    side-menu: center footer itens

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -7,20 +7,16 @@ import zenithLogo from '../public/images/logo-zenith.png'
 export default function Footer() {
     return (
         <div className={styles.footerContainer}>
-            <div className={styles.portrait}>
-                <img
-                    src="/images/logo-zenith.png"
-                    alt="Zenith logo"
-                    className={styles.image}  
-                />
-            </div>
-            <div className={styles.portrait}>
-                <img
-                    src="/images/logo-obsat.png"
-                    alt="OBSAT logo"
-                    className={styles.image}  
-                />
-            </div>
+            <img
+                src="/images/logo-zenith.png"
+                alt="Zenith logo"
+                className={styles.image}  
+            />
+            <img
+                src="/images/logo-obsat.png"
+                alt="OBSAT logo"
+                className={styles.image}  
+            />
         </div>
     );
 }
