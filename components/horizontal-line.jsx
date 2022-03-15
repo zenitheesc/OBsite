98d586c62cb15f397a93bab86f5c222abf2e75b0
Arthur Grillo <arthur.grillo@usp.br>
2022-03-14 22:56:53 -0300

    side-menu: add horizontalLine
    Co-authored-by: MichelH4cker <michel.hecker@usp.br>
    Co-authored-by: Pedro Torrente <pedrotorrente@usp.br>

---
+++ components/horizontal-line.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import styles from "../styles/horizontal-line.module.css";
+
+export default function HorizontalLine() {
+  return <hr className={styles.horizontalLine} />;
+}
