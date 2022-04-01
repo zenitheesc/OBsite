899d662f4a18fc6223385c451fb5c1acde166f70
Angelo Guido <angelobguido@gmail.com>
2022-03-31 22:22:51 -0300

    binary: Create mockup for firebase query

---
+++ pages/api/binary.js
@@ -0,0 +1,3 @@
+export default function FIREBASE_BINARY_MOCKUP(req, res) {
+    res.status(200).json({ byteArray : [64, 0, 123, 0, 31, 2, 115, 116, 114, 105, 110, 103, 0, 16, 43, 32, 64, 0, 0, 0, 0, 0, 0, 0, 0, 0] });
+}
