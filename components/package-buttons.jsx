973d6f6c53c1e771b68950c56eb924290be5e633
Math-42 <matheus.v4200@gmail.com>
2022-05-09 17:04:24 -0300

    packages: add support to select and unselect packages

--- components/package-buttons.jsx
+++ components/package-buttons.jsx
@@ -1,6 +1,61 @@
 import React from "react";
+import { useState, useRef, useEffect } from "react";
 import styles from "../styles/packages.module.css";
 
-export default function PackageButtons({ time }) {
-  return <div className={styles.packageButton}> {time} </div>;
+export default function PackageButtons(props) {
+  const packageData = props.package;
+  const { onAdd, onRemove } = props;
+  const [isSelected, selectionSetter] = useState(false);
+  const timer = useRef();
+
+  const toggleSelection = () => {
+    const action = isSelected ? onRemove : onAdd;
+    action(packageData);
+    selectionSetter(!isSelected);
+  };
+
+  const select = () => {
+    onAdd(packageData);
+    selectionSetter(true);
+  };
+
+  const unselect = () => {
+    onRemove(packageData);
+    selectionSetter(false);
+  };
+
+  const toggleAllSelections = () => {
+    const eventName = isSelected ? "unselect-all" : "select-all";
+    window.dispatchEvent(new CustomEvent(eventName));
+  };
+
+  const onClickHandler = (event) => {
+    clearTimeout(timer.current);
+
+    if (event.detail === 1) {
+      timer.current = setTimeout(toggleSelection, 200);
+    } else if (event.detail === 2) {
+      toggleAllSelections();
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("select-all", select, false);
+    window.addEventListener("unselect-all", unselect, false);
+    return () => {
+      window.removeEventListener("select-all", select, false);
+      window.removeEventListener("unselect-all", unselect, false);
+    };
+  }, []);
+
+  return (
+    <div
+      onClick={onClickHandler}
+      className={
+        isSelected ? styles.packageButtonSelected : styles.packageButton
+      }
+    >
+      {packageData["time-stamp"]}
+    </div>
+  );
 }
--- components/package-grid.jsx
+++ components/package-grid.jsx
@@ -4,7 +4,7 @@ import styles from "../styles/packages.module.css";
 export default function PackageGrid({ children }) {
   return (
     <div className={styles.wrapperGrid}>
-      <div className={styles.title}>Dados Recebidos</div>
+      <div className={styles.title}> Pacotes recebidos</div>
       <div className={styles.contentGrid}> {children} </div>
     </div>
   );
