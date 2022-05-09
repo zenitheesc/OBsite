dc46a6a16bb73734cdb39ffc689b59562da2639f
Math-42 <matheus.v4200@gmail.com>
2022-05-09 17:07:22 -0300

    index: defining principal states on index page

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,29 @@
 import React from "react";
 import SideMenu from "../components/side-menu";
 import styles from "../styles/home.module.css";
+import DataView from "../components/data-view";
+import { useState } from "react";
 
 export default function Home() {
+  const [teamId, teamIdSetter] = useState(0);
+  const [jsonExample, jsonExampleSetter] = useState();
+  const [selectedPackages, selectedPackagesSetter] = useState([]);
+
   return (
     <div className={styles.container}>
-      <SideMenu />;
+      <SideMenu
+        teamId={teamId}
+        teamIdSetter={teamIdSetter}
+        jsonExample={jsonExample}
+        jsonExampleSetter={jsonExampleSetter}
+        selectedPackages={selectedPackages}
+        selectedPackagesSetter={selectedPackagesSetter}
+      />
+      <DataView
+        teamId={teamId}
+        jsonExample={jsonExample}
+        selectedPackages={selectedPackages}
+      />
     </div>
   );
 }
